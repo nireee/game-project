80eea8a356433f87deb448b23421bae3a81022c6
winchell459 <johncmorris@gmail.com>
2021-02-10 15:29:17 -0800

    end of day 2/3/21

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    [SerializeField] private int PieceId = 0;
+    Vector2 displacement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        displacement = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void OnMouseDrag()
+    {
+        print("hello");
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        print(Input.mousePosition + " -> " + mousePos);
+        transform.position = new Vector3(mousePos.x, mousePos.y, transform.position.z) + (Vector3) displacement;
+    }
+
+}
