73956b010f37dad6e6bbeb42c5c6c3a3adc7ed85
winchell459 <johncmorris@gmail.com>
2021-05-05 18:37:21 -0700

    end of day 5/5/21

---
+++ Assets/Scripts/pipePuzzle/Pipe.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    public bool isSource;
+    public bool hasWater;
+    public Transform[] PipeOpenings;
+
+    public Sprite Empty;
+    public Sprite Full;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        setSprite();
+    }
+
+    private void setSprite()
+    {
+        if (hasWater) GetComponent<SpriteRenderer>().sprite = Full;
+        else GetComponent<SpriteRenderer>().sprite = Empty;
+    }
+
+    public bool CheckConnection(Vector2 connectionDir)
+    {
+        if (!hasWater) return false;
+        foreach(Transform opening in PipeOpenings)
+        {
+            Vector2 dir = opening.up;
+            print(connectionDir + " " + dir);
+            if (-dir == connectionDir)
+            {
+
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/pipePuzzle/PipePuzzle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipePuzzle : MonoBehaviour
+{
+    public int Width = 3;
+    public int Height = 4;
+    public List<Pipe> pipes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Pipe pipe in pipes)
+        {
+            pipe.hasWater = checkForWater(pipe);
+
+        }
+    }
+
+    public bool checkForWater(Pipe pipe)
+    {
+        if (pipe.isSource) return true;
+        int pipeID = pipes.IndexOf(pipe);
+        int x = pipeID % Width;
+        int y = pipeID / Width;
+        if (checkConnection(pipe, x, y, new Vector2(1, 0))) return true;
+        if (checkConnection(pipe, x, y, new Vector2(-1, 0))) return true;
+        if (checkConnection(pipe, x, y, new Vector2(0, 1))) return true;
+        if (checkConnection(pipe, x, y, new Vector2(0, -1))) return true;
+        return false;
+    }
+
+    public Pipe getPipe(int x, int y)
+    {
+        if (x >= 0 && x < Width && y >= 0 && y < Height)
+        {
+            int pipeID = x + y * Width;
+            return pipes[pipeID];
+        }
+        else
+            return null;
+    }
+
+    private bool checkConnection(Pipe pipe, int x, int y, Vector2 dir)
+    {
+        bool hasMatchingOpening = false;
+        foreach(Transform opening in pipe.PipeOpenings)
+        {
+            //print(opening.up + " " + dir);
+            if((int)opening.up.y == (int)dir.y && (int)opening.up.x == (int)dir.x)
+            {
+                hasMatchingOpening = true;
+            }
+        }
+
+        if (hasMatchingOpening)
+        {
+            //flip the y dir 
+            Pipe neighbor = getPipe(x + (int)dir.x, y - (int)dir.y);
+            if (neighbor) return neighbor.CheckConnection(dir);
+        }
+        
+        return false;
+    }
+}
